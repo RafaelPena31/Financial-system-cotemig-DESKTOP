b3c284d581f1b0c14ca79e9279e827f69c90e9d4
Rafael Pena <rafaelppena31@gmail.com>
2020-11-28 15:14:54 -0300

    Progress on Expense Screen

--- sistema_finaneiro/Update_Expense.Designer.cs
+++ sistema_finaneiro/Update_Expense.Designer.cs
@@ -30,19 +30,21 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Update_Expense));
             this.btnUptadeExpenseReturn = new System.Windows.Forms.Button();
-            this.txtUpdateValueExpense = new System.Windows.Forms.TextBox();
             this.dtpUpdateDateExpense = new System.Windows.Forms.DateTimePicker();
             this.btnUpdateExpense = new System.Windows.Forms.Button();
             this.lblUpdateValueExpense = new System.Windows.Forms.Label();
             this.cbxUpdateNameExpense = new System.Windows.Forms.ComboBox();
             this.lblUpdateChooseDateExpense = new System.Windows.Forms.Label();
             this.lblUpdateNameExpense = new System.Windows.Forms.Label();
+            this.txtUpdateExpenseId = new System.Windows.Forms.TextBox();
+            this.txtUpdateExpenseValue = new System.Windows.Forms.TextBox();
+            this.lblUpdateExpenseId = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnUptadeExpenseReturn
             // 
             this.btnUptadeExpenseReturn.BackColor = System.Drawing.Color.White;
-            this.btnUptadeExpenseReturn.Font = new System.Drawing.Font("Open Sans", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUptadeExpenseReturn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnUptadeExpenseReturn.Image = ((System.Drawing.Image)(resources.GetObject("btnUptadeExpenseReturn.Image")));
             this.btnUptadeExpenseReturn.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.btnUptadeExpenseReturn.Location = new System.Drawing.Point(43, 283);
@@ -54,27 +56,22 @@
             this.btnUptadeExpenseReturn.Text = "Voltar";
             this.btnUptadeExpenseReturn.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnUptadeExpenseReturn.UseVisualStyleBackColor = false;
-            // 
-            // txtUpdateValueExpense
-            // 
-            this.txtUpdateValueExpense.Location = new System.Drawing.Point(43, 178);
-            this.txtUpdateValueExpense.Name = "txtUpdateValueExpense";
-            this.txtUpdateValueExpense.Size = new System.Drawing.Size(269, 20);
-            this.txtUpdateValueExpense.TabIndex = 59;
+            this.btnUptadeExpenseReturn.Click += new System.EventHandler(this.btnUptadeExpenseReturn_Click);
             // 
             // dtpUpdateDateExpense
             // 
-            this.dtpUpdateDateExpense.Font = new System.Drawing.Font("Open Sans", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpUpdateDateExpense.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpUpdateDateExpense.Format = System.Windows.Forms.DateTimePickerFormat.Short;
             this.dtpUpdateDateExpense.Location = new System.Drawing.Point(43, 121);
             this.dtpUpdateDateExpense.Name = "dtpUpdateDateExpense";
-            this.dtpUpdateDateExpense.Size = new System.Drawing.Size(269, 22);
+            this.dtpUpdateDateExpense.Size = new System.Drawing.Size(269, 23);
             this.dtpUpdateDateExpense.TabIndex = 58;
             // 
             // btnUpdateExpense
             // 
             this.btnUpdateExpense.AutoEllipsis = true;
             this.btnUpdateExpense.BackColor = System.Drawing.Color.White;
-            this.btnUpdateExpense.Font = new System.Drawing.Font("Open Sans", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdateExpense.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnUpdateExpense.Image = ((System.Drawing.Image)(resources.GetObject("btnUpdateExpense.Image")));
             this.btnUpdateExpense.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.btnUpdateExpense.Location = new System.Drawing.Point(193, 283);
@@ -90,12 +87,12 @@
             // lblUpdateValueExpense
             // 
             this.lblUpdateValueExpense.AutoSize = true;
-            this.lblUpdateValueExpense.Font = new System.Drawing.Font("Open Sans", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUpdateValueExpense.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblUpdateValueExpense.Location = new System.Drawing.Point(43, 156);
             this.lblUpdateValueExpense.Name = "lblUpdateValueExpense";
-            this.lblUpdateValueExpense.Size = new System.Drawing.Size(87, 15);
+            this.lblUpdateValueExpense.Size = new System.Drawing.Size(119, 17);
             this.lblUpdateValueExpense.TabIndex = 56;
-            this.lblUpdateValueExpense.Text = "Valor da receita";
+            this.lblUpdateValueExpense.Text = "Valor da despesa";
             // 
             // cbxUpdateNameExpense
             // 
@@ -108,30 +105,60 @@
             // lblUpdateChooseDateExpense
             // 
             this.lblUpdateChooseDateExpense.AutoSize = true;
-            this.lblUpdateChooseDateExpense.Font = new System.Drawing.Font("Open Sans", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUpdateChooseDateExpense.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblUpdateChooseDateExpense.Location = new System.Drawing.Point(43, 100);
             this.lblUpdateChooseDateExpense.Name = "lblUpdateChooseDateExpense";
-            this.lblUpdateChooseDateExpense.Size = new System.Drawing.Size(81, 15);
+            this.lblUpdateChooseDateExpense.Size = new System.Drawing.Size(102, 17);
             this.lblUpdateChooseDateExpense.TabIndex = 54;
             this.lblUpdateChooseDateExpense.Text = "Escolha a data";
             // 
             // lblUpdateNameExpense
             // 
             this.lblUpdateNameExpense.AutoSize = true;
-            this.lblUpdateNameExpense.Font = new System.Drawing.Font("Open Sans", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUpdateNameExpense.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblUpdateNameExpense.Location = new System.Drawing.Point(43, 42);
             this.lblUpdateNameExpense.Name = "lblUpdateNameExpense";
-            this.lblUpdateNameExpense.Size = new System.Drawing.Size(91, 15);
+            this.lblUpdateNameExpense.Size = new System.Drawing.Size(112, 17);
             this.lblUpdateNameExpense.TabIndex = 53;
             this.lblUpdateNameExpense.Text = "Nome da receita";
+            // 
+            // txtUpdateExpenseId
+            // 
+            this.txtUpdateExpenseId.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUpdateExpenseId.Location = new System.Drawing.Point(46, 234);
+            this.txtUpdateExpenseId.Name = "txtUpdateExpenseId";
+            this.txtUpdateExpenseId.Size = new System.Drawing.Size(269, 23);
+            this.txtUpdateExpenseId.TabIndex = 61;
+            this.txtUpdateExpenseId.TextChanged += new System.EventHandler(this.txtUpdateExpenseId_TextChanged);
+            // 
+            // txtUpdateExpenseValue
+            // 
+            this.txtUpdateExpenseValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUpdateExpenseValue.Location = new System.Drawing.Point(45, 177);
+            this.txtUpdateExpenseValue.Name = "txtUpdateExpenseValue";
+            this.txtUpdateExpenseValue.Size = new System.Drawing.Size(269, 23);
+            this.txtUpdateExpenseValue.TabIndex = 62;
+            // 
+            // lblUpdateExpenseId
+            // 
+            this.lblUpdateExpenseId.AutoSize = true;
+            this.lblUpdateExpenseId.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUpdateExpenseId.Location = new System.Drawing.Point(43, 214);
+            this.lblUpdateExpenseId.Name = "lblUpdateExpenseId";
+            this.lblUpdateExpenseId.Size = new System.Drawing.Size(97, 17);
+            this.lblUpdateExpenseId.TabIndex = 63;
+            this.lblUpdateExpenseId.Text = "Id da despesa";
+            this.lblUpdateExpenseId.Click += new System.EventHandler(this.lblUpdateExpenseId_Click);
             // 
             // Update_Expense
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(358, 402);
+            this.Controls.Add(this.lblUpdateExpenseId);
+            this.Controls.Add(this.txtUpdateExpenseValue);
+            this.Controls.Add(this.txtUpdateExpenseId);
             this.Controls.Add(this.btnUptadeExpenseReturn);
-            this.Controls.Add(this.txtUpdateValueExpense);
             this.Controls.Add(this.dtpUpdateDateExpense);
             this.Controls.Add(this.btnUpdateExpense);
             this.Controls.Add(this.lblUpdateValueExpense);
@@ -139,7 +166,8 @@
             this.Controls.Add(this.lblUpdateChooseDateExpense);
             this.Controls.Add(this.lblUpdateNameExpense);
             this.Name = "Update_Expense";
-            this.Text = "Update_Expense";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Alterar despesa";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -148,12 +176,14 @@
         #endregion
 
         private System.Windows.Forms.Button btnUptadeExpenseReturn;
-        private System.Windows.Forms.TextBox txtUpdateValueExpense;
         private System.Windows.Forms.DateTimePicker dtpUpdateDateExpense;
         private System.Windows.Forms.Button btnUpdateExpense;
         private System.Windows.Forms.Label lblUpdateValueExpense;
         private System.Windows.Forms.ComboBox cbxUpdateNameExpense;
         private System.Windows.Forms.Label lblUpdateChooseDateExpense;
         private System.Windows.Forms.Label lblUpdateNameExpense;
+        private System.Windows.Forms.TextBox txtUpdateExpenseId;
+        private System.Windows.Forms.TextBox txtUpdateExpenseValue;
+        private System.Windows.Forms.Label lblUpdateExpenseId;
     }
 }
--- sistema_finaneiro/Update_Expense.cs
+++ sistema_finaneiro/Update_Expense.cs
@@ -16,5 +16,36 @@ namespace sistema_finaneiro
         {
             InitializeComponent();
         }
+
+        User UserClass = new User();
+        string sql;
+
+        private void btnUptadeExpenseReturn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void lblUpdateExpenseId_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtUpdateExpenseId_TextChanged(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+            if (txtUpdateExpenseId.Text != "")
+            {
+                sql = string.Format("select * from Category where id = '{0}' and User_id = '{1}'", int.Parse(txtUpdateExpenseId.Text),
+                UserClass.getUserId());
+                dt = UserClass.DataList(sql);
+
+                if (dt.Rows.Count > 0)
+                {
+                    cbxUpdateNameExpense.Text = dt.Rows[0]["name"].ToString();
+                    dtpUpdateDateExpense.Text = dt.Rows[0]["data"].ToString();
+                    txtUpdateExpenseValue.Text = dt.Rows[0]["value"].ToString();
+                }
+            }
+        }
     }
 }
