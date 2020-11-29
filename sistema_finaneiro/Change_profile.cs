f0192e342a7f977557ad99f0becb36ae712608f1
Rafael Pena <rafaelppena31@gmail.com>
2020-11-29 03:27:36 -0300

    Finish Change_profile

--- sistema_finaneiro/Change_profile.cs
+++ sistema_finaneiro/Change_profile.cs
@@ -20,26 +20,38 @@ namespace sistema_finaneiro
         User UserClass = new User();
         string sql;
 
-        public void limpar()
+        public void CbxList()
         {
-            txtAdressUpdateProfile.Clear();
-            txtEmailUpdateProfile.Clear();
-            txtUpdateNameProfile.Clear();
-            txtTelephoneUpdateProfile.Clear();
-            textPasswordUpdateProfile.Clear();
-            cbxSexUpdateProfile.SelectedIndex = -1;
+            DataTable cbxItemList = new DataTable();
+            cbxItemList.Columns.Add("name", typeof(string));
+            cbxItemList.Columns.Add("value", typeof(string));
+            cbxItemList.Rows.Add("Masculino", "M");
+            cbxItemList.Rows.Add("Feminino", "F");
+            cbxItemList.Rows.Add("Outro", "O");
+
+            cbxSexUpdateProfile.DataSource = cbxItemList;
+            cbxSexUpdateProfile.DisplayMember = "name";
+            cbxSexUpdateProfile.ValueMember = "value";
         }
 
         private void btnUserChange_Click(object sender, EventArgs e)
         {
-            sql = string.Format("update User set name = '{0}', email = '{1}', genre = '{2}', phone = '{3}', address = '{4}', password = '{5}' where id = '{6}'", txtUpdateNameProfile.Text,
-                txtEmailUpdateProfile.Text, cbxSexUpdateProfile.Text, txtTelephoneUpdateProfile.Text, txtAdressUpdateProfile.Text, textPasswordUpdateProfile.Text, UserClass.getUserId());
-            UserClass.UpdateData(sql);
-            MessageBox.Show("Dados atualizados com sucesso!", "Atualização", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            if (txtUpdateNameProfile.Text != "" && txtEmailUpdateProfile.Text != "" && cbxSexUpdateProfile.Text != "" && txtTelephoneUpdateProfile.Text != "" && txtAdressUpdateProfile.Text != "" && textPasswordUpdateProfile.Text != "")
+            {
+                sql = string.Format("update User set name = '{0}', email = '{1}', genre = '{2}', phone = '{3}', address = '{4}', password = '{5}' where id = '{6}'", txtUpdateNameProfile.Text,
+                txtEmailUpdateProfile.Text, cbxSexUpdateProfile.SelectedValue.ToString(), txtTelephoneUpdateProfile.Text, txtAdressUpdateProfile.Text, textPasswordUpdateProfile.Text, UserClass.getUserId());
+                UserClass.UpdateData(sql);
+                MessageBox.Show("Dados atualizados com sucesso!", "Atualização", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Close();
+            } else
+            {
+                MessageBox.Show("Preencha todos os campos para continuar.", "Perfil", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
         }
 
         private void Change_profile_Load(object sender, EventArgs e)
         {
+            CbxList();
             DataTable dt = new DataTable();
             sql = string.Format("select * from User where id = '{0}'", UserClass.getUserId());
             dt = UserClass.DataList(sql);
@@ -48,7 +60,7 @@ namespace sistema_finaneiro
             {
                 txtUpdateNameProfile.Text = dt.Rows[0]["name"].ToString();
                 txtEmailUpdateProfile.Text = dt.Rows[0]["email"].ToString();
-                cbxSexUpdateProfile.Text = dt.Rows[0]["genre"].ToString();
+                cbxSexUpdateProfile.SelectedValue = dt.Rows[0]["genre"].ToString();
                 txtTelephoneUpdateProfile.Text = dt.Rows[0]["phone"].ToString();
                 txtAdressUpdateProfile.Text = dt.Rows[0]["address"].ToString();
                 textPasswordUpdateProfile.Text = dt.Rows[0]["password"].ToString();
